fed544bf60fca85953e3241a99fadb48c0a8d322
veronicapava <veronicapava@gmail.com>
2022-03-16 22:42:06 -0500

    Modifica nombre de parametros

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ function multiplicar(num1, num2) {
     return arr.reduce((acc, act) => sumar(acc, act))
 }
 
-function dividir(num1, num2) {
+function dividir(dividendo, divisor) {
 
-    if (num2 == 0) { throw new Error("No se puede dividir por cero") }
-    if (num1 == 0) return 0
-    if (num2 > num1) { throw new Error("No se puede dividir así, solo divisiones enteras") }
+    if (divisor == 0) { throw new Error("No se puede dividir por cero") }
+    if (dividendo == 0) return 0
+    if (divisor > dividendo) { throw new Error("No se puede dividir así, solo divisiones enteras") }
 
-    const arr = Array.from(Array(num1).keys())
-    return arr.find(x => multiplicar(x, num2) > restar(num1, num2) && multiplicar(x, num2) < sumar(num1, num2))
+    const arr = Array.from(Array(dividendo).keys())
+    return arr.find(x => multiplicar(x, divisor) > restar(dividendo, divisor) && multiplicar(x, divisor) < sumar(dividendo, divisor))
 
 }
 
